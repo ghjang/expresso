addf8a7f6a98c6b8f823d5097b2af06434d180e8
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-07-05 21:01:07 +0900

    added metafunctions to check the node types: - is_terminal - is_non_terminal

--- include/expresso/expression.h
+++ include/expresso/expression.h
@@ -2,6 +2,9 @@
 #define EXPRESSO_EXPRESSION_H
 
 
+#include <type_traits>
+
+
 namespace expresso
 {
     struct terminal
@@ -10,6 +13,39 @@ namespace expresso
     template <typename Left, typename OpTag, typename Right>
     struct non_terminal
     { };
+
+
+    template <typename T>
+    struct is_terminal
+            : std::conditional_t<
+                    std::is_same<std::decay_t<T>, terminal>::value,
+                    std::true_type,
+                    std::false_type
+              >
+    { };
+
+    template <typename T>
+    inline constexpr auto is_terminal_v = is_terminal<T>::value;
+
+
+    namespace detail
+    {
+        template <typename T>
+        struct is_non_terminal_impl : std::false_type
+        { };
+
+        template <typename Left, typename OpTag, typename Right>
+        struct is_non_terminal_impl<non_terminal<Left, OpTag, Right>>
+                    : std::true_type
+        { };
+    } // namespace detail
+
+    template <typename T>
+    struct is_non_terminal : detail::is_non_terminal_impl<std::decay_t<T>>
+    { };
+
+    template <typename T>
+    inline constexpr auto is_non_terminal_v = is_non_terminal<T>::value;
 } // namespace expresso
 
 
--- test/expresso_test.cpp
+++ test/expresso_test.cpp
@@ -8,16 +8,35 @@ using namespace expresso;
 
 TEST_CASE("equal_to expressions", "[expresso]")
 {
+    static_assert(is_terminal_v<decltype(_1)>);
+
     auto expr0 = 1 == 1;
     static_assert(std::is_same<bool, decltype(expr0)>());
 
     auto expr1 = _1 == 1;
     auto expr2 = 1 == _1;
     auto expr3 = _1 == _1;
+    static_assert(is_non_terminal_v<decltype(expr1)>);
+    static_assert(is_non_terminal_v<decltype(expr2)>);
+    static_assert(is_non_terminal_v<decltype(expr3)>);
 
     auto expr4 = _1 == 1 == 2;
     auto expr5 = 1 == (_1 == 2);
+    static_assert(is_non_terminal_v<decltype(expr4)>);
+    static_assert(is_non_terminal_v<decltype(expr5)>);
 
+    // both sides are lvalue non_terminal.
     auto expr6 = expr4 == expr5;
+    static_assert(is_non_terminal_v<decltype(expr6)>);
+
+    auto const expr7 = expr6 == _1;
+    static_assert(is_non_terminal_v<decltype(expr7)>);
 }
 
+TEST_CASE("complex expressions", "[expresso]")
+{
+    //auto expr0 = _1 + 1 && _2;
+
+    // both sides are rvalue non_terminal.
+    //auto expr1 = _1 + 1 && _2 - 2;
+}
